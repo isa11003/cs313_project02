1a3ad29c86e7aa4881a9d69b4b07b97a839ab31d
isa11003 <isa11003@byui.edu>
2018-03-15 10:27:15 -0600

    put everything in index.js instead of server.js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .get('/test', (req, res) => {
+	  res.render("testing");
+  })
+  .get('/getPerson', (req, res) => {
+	  client.query('SELECT * FROM person', (err, resp) => {
+		  done();
+		  res.render("database");
+		  })
+  })
   .get('/post', (req, res) => {
 	 var weight = Number(req.query.weight);
 	 var postage = req.query.postage;
