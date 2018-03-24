6287d20db9c70b69227b626671e0fbff6d62da6e
isa11003 <isa11003@byui.edu>
2018-03-24 16:26:26 -0600

    debugging

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ express()
 					if (err)
 						console.log("failed to retrieve new person");
 					else{
-						personId = result.rows[0];
+						personId = JSON.parse(result.rows[0]);
 						console.log("person id received: " + personId);
 					}
 				});
